e3ae48776b52b257e6bb3902c7b70c10008cc712
Fredx <freddy87@gmail.com>
2020-01-10 23:45:12 +0100

    Redux toolkit setup

---
+++ src/rootReducer.ts
@@ -0,0 +1,5 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+export const rootReducer = combineReducers({});
+
+export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer";
+
+export const store = configureStore({
+  reducer: rootReducer
+});
+
+export type AppDispatch = typeof store.dispatch;
