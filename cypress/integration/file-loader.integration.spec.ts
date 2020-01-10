277b7289e2f63b671cbe4e1a8a061530a737a135
Fredx <freddy87@gmail.com>
2020-01-10 22:57:23 +0100

    added valid api parsing test

--- cypress/integration/file-loader.integration.spec.ts
+++ cypress/integration/file-loader.integration.spec.ts
@@ -18,4 +18,18 @@ describe("OpenAPI file loading and parsing", () => {
 
     expectMessage("error", /invalid/i);
   });
+
+  it("should return success message when uploading valid YAML file", () => {
+    cy.fixture("petstore.yaml").then(fileContent => {
+      cy.contains(`[role="button"]`, /load openapi/i).within(() => {
+        cy.get("input").upload({
+          fileContent,
+          fileName: "openapi-invalid.json",
+          mimeType: "application/json"
+        });
+      });
+    });
+
+    expectMessage("success", /parsed.*success/i);
+  });
 });
