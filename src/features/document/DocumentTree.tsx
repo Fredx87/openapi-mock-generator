4c25fd8ed11db98ae9ef1fabbedea8ee53980891
Fredx <freddy87@gmail.com>
2020-01-15 01:15:37 +0100

    Refactored Document Tree with custom render

--- src/features/document/DocumentTree.tsx
+++ src/features/document/DocumentTree.tsx
@@ -2,13 +2,27 @@ import { Tree } from "antd";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../rootReducer";
+import { GeneralTreeNode } from "./tree-builder";
+
+const { TreeNode } = Tree;
+
+function renderNode(node: GeneralTreeNode): React.ReactNode {
+  if (node.type === "Branch") {
+    return (
+      <TreeNode selectable={false} {...node}>
+        {node.children.map(child => renderNode(child))}
+      </TreeNode>
+    );
+  }
+  return <TreeNode {...node}></TreeNode>;
+}
 
 export const DocumentTree: React.FC = () => {
   const treeData = useSelector((state: RootState) => state.document.tree);
 
   return (
     <div data-testid="document-tree">
-      <Tree treeData={treeData}></Tree>
+      <Tree>{treeData.map(child => renderNode(child))}</Tree>
     </div>
   );
 };
