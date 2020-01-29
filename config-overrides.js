a8c55a69fa437c7e028ddc7a9ab583e29ffea082
Fredx <freddy87@gmail.com>
2020-01-29 01:40:48 +0100

    Added babel plugin import for ant

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,15 @@
-const { addWebpackPlugin, override } = require("customize-cra");
+const {
+  addWebpackPlugin,
+  override,
+  fixBabelImports
+} = require("customize-cra");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css"
+  }),
   addWebpackPlugin(new MonacoWebpackPlugin({ languages: ["json"] }))
 );
