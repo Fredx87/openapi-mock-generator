09388ea2e63ba0322ef4bbffd41bea8d7d8e5cab
Fredx <freddy87@gmail.com>
2020-02-08 17:56:01 +0100

    Added project creation interface

--- cypress/integration/projects.integration.spec.ts
+++ cypress/integration/projects.integration.spec.ts
@@ -1,8 +1,67 @@
-import { EMPTY_MESSAGE } from "features/project/ProjectsList";
+import {
+  CREATE_PROJECT_MSG,
+  EMPTY_MSG,
+  NEW_PROJECT_NAME_PLACEHOLDER
+} from "features/project/ProjectsList";
 
 describe("Projects Management", () => {
-  it("should show empty data message with empty database", () => {
+  beforeEach(() => {
     cy.visit("/");
-    cy.findByText(EMPTY_MESSAGE).should("exist");
+  });
+
+  it("should show empty data message with empty database", () => {
+    cy.findByText(EMPTY_MSG).should("exist");
+  });
+
+  it("should create a new project when enter pressed and name inserted", () => {
+    cy.findByText(CREATE_PROJECT_MSG).click({ force: true });
+
+    cy.findByText(EMPTY_MSG).should("not.exist");
+
+    cy.findByPlaceholderText(NEW_PROJECT_NAME_PLACEHOLDER).type(
+      "New Project{enter}"
+    );
+
+    cy.findByText(EMPTY_MSG).should("not.exist");
+
+    cy.findByText("New Project").should("exist");
+  });
+
+  it("should create a new project when input blurred and name inserted", () => {
+    cy.findByText(CREATE_PROJECT_MSG).click({ force: true });
+
+    cy.findByText(EMPTY_MSG).should("not.exist");
+
+    cy.findByPlaceholderText(NEW_PROJECT_NAME_PLACEHOLDER)
+      .type("New Project")
+      .blur();
+
+    cy.findByText(EMPTY_MSG).should("not.exist");
+
+    cy.findByText("New Project").should("exist");
+  });
+
+  it("should not create a new project when enter pressed and name empty", () => {
+    cy.findByText(CREATE_PROJECT_MSG).click({ force: true });
+
+    cy.findByText(EMPTY_MSG).should("not.exist");
+
+    cy.findByPlaceholderText(NEW_PROJECT_NAME_PLACEHOLDER).type("{enter}");
+
+    cy.findByText(EMPTY_MSG).should("exist");
+
+    cy.findByText("New Project").should("not.exist");
+  });
+
+  it("should not create a new project when input blurred and name empty", () => {
+    cy.findByText(CREATE_PROJECT_MSG).click({ force: true });
+
+    cy.findByText(EMPTY_MSG).should("not.exist");
+
+    cy.findByPlaceholderText(NEW_PROJECT_NAME_PLACEHOLDER).blur();
+
+    cy.findByText(EMPTY_MSG).should("exist");
+
+    cy.findByText("New Project").should("not.exist");
   });
 });
--- src/features/project/ProjectsList.tsx
+++ src/features/project/ProjectsList.tsx
@@ -1,20 +1,116 @@
+import Button from "antd/es/button";
 import Empty from "antd/es/empty";
+import Input from "antd/es/input";
 import Layout from "antd/es/layout";
+import Table, { ColumnProps } from "antd/es/table";
 import React from "react";
 import styled from "styled-components";
+import { useImmer } from "use-immer";
+
+interface Project {
+  key: number;
+  name: string;
+  createdAt: Date;
+  modifiedAt: Date;
+}
+
+interface EditableProject extends Project {
+  isEditing: boolean;
+}
 
 const { Content } = Layout;
 
 const StyledContent = styled(Content)`
-  padding: 50px 0;
+  padding: 50px 20px;
+  max-width: 1200px;
+  text-align: center;
 `;
 
-export const EMPTY_MESSAGE = "No project found. Please create a new project.";
+export const EMPTY_MSG = "No project found. Please create a new project.";
+export const CREATE_PROJECT_MSG = "Create New Project";
+export const NEW_PROJECT_NAME_PLACEHOLDER = "New Project Name";
+
+function renderDate(date: Date): string {
+  return Intl.DateTimeFormat().format(date);
+}
 
 export const ProjectsList: React.FC = () => {
+  const [dataSource, updateDataSource] = useImmer<EditableProject[]>([]);
+
+  const columns: ColumnProps<EditableProject>[] = [
+    {
+      title: "Project name",
+      dataIndex: "name",
+      key: "name",
+      render: (_, record, index) => {
+        if (record.isEditing) {
+          return (
+            <Input
+              autoFocus
+              placeholder={NEW_PROJECT_NAME_PLACEHOLDER}
+              onPressEnter={e => {
+                onProjectNameChanged(e.currentTarget.value, index);
+              }}
+              onBlur={e => {
+                onProjectNameChanged(e.currentTarget.value, index);
+              }}
+            />
+          );
+        } else {
+          return record.name;
+        }
+      }
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: renderDate
+    },
+    {
+      title: "Modified At",
+      dataIndex: "modifiedAt",
+      key: "modifiedAt",
+      render: renderDate
+    }
+  ];
+
+  const onCreateProject = () => {
+    const newProject: EditableProject = {
+      key: -1,
+      name: "",
+      createdAt: new Date(),
+      modifiedAt: new Date(),
+      isEditing: true
+    };
+    updateDataSource(draft => {
+      draft.push(newProject);
+    });
+  };
+
+  const onProjectNameChanged = (value: string, index: number) => {
+    if (value) {
+      updateDataSource(draft => {
+        draft[index].name = value;
+        draft[index].isEditing = false;
+      });
+    } else {
+      updateDataSource(draft => {
+        draft.splice(index, 1);
+      });
+    }
+  };
+
   return (
     <StyledContent>
-      <Empty description={EMPTY_MESSAGE} />
+      <Button type="primary" size="large" onClick={onCreateProject}>
+        {CREATE_PROJECT_MSG}
+      </Button>
+      <Table<EditableProject>
+        dataSource={dataSource}
+        columns={columns}
+        locale={{ emptyText: <Empty description={EMPTY_MSG} /> }}
+      ></Table>
     </StyledContent>
   );
 };
