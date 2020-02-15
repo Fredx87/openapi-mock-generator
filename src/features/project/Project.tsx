1abee3d571066f1cbce04accc347dd9c8f0f972d
Fredx <freddy87@gmail.com>
2020-02-16 00:50:54 +0100

    Refactored project sidebar structure

--- src/features/project/Project.tsx
+++ src/features/project/Project.tsx
@@ -4,52 +4,40 @@ import Skeleton from "antd/es/skeleton";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
-import { DocumentTree } from "src/features/document/DocumentTree";
 import { RootState } from "src/rootReducer";
 import styled from "styled-components";
 import { EMPTY_PROJECT_MSG } from "./constants";
-import { OpenApiLoader } from "./OpenApiLoader";
+import { ProjectSider } from "./ProjectSider";
 
 const EditorsWrapper = React.lazy(() =>
   import("src/features/editor/EditorsWrapper")
 );
 
-const { Sider, Content } = Layout;
-
-const StyledContent = styled(Content)`
-  text-align: center;
-`;
+const { Content } = Layout;
 
 const StyledEmpty = styled(Empty)`
   padding: 30px 0;
-`;
-
-const StyledSider = styled(Sider)`
-  overflow: auto;
 `;
 
 export const Project: React.FC = () => {
   const document = useSelector((state: RootState) => state.document);
   const { path } = useRouteMatch();
 
-  return document.status === "empty" ? (
-    <StyledContent>
-      <StyledEmpty description={EMPTY_PROJECT_MSG}></StyledEmpty>
-      <OpenApiLoader />
-    </StyledContent>
-  ) : (
+  return (
     <Layout>
-      <StyledSider theme="light" width={300}>
-        <DocumentTree tree={document.tree}></DocumentTree>
-      </StyledSider>
+      <ProjectSider document={document} />
       <Content>
-        <Switch>
-          <Route path={`${path}/:referenceName`}>
-            <React.Suspense fallback={<Skeleton />}>
-              <EditorsWrapper></EditorsWrapper>
-            </React.Suspense>
-          </Route>
-        </Switch>
+        {document.status === "empty" ? (
+          <StyledEmpty description={EMPTY_PROJECT_MSG} />
+        ) : (
+          <Switch>
+            <Route path={`${path}/:referenceName`}>
+              <React.Suspense fallback={<Skeleton />}>
+                <EditorsWrapper />
+              </React.Suspense>
+            </Route>
+          </Switch>
+        )}
       </Content>
     </Layout>
   );
