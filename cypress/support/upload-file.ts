01aa3f4c29691deeafee60b93fbb0e31490a8965
Fredx <freddy87@gmail.com>
2020-02-15 01:53:02 +0100

    fixed failing test

--- cypress/support/upload-file.ts
+++ cypress/support/upload-file.ts
@@ -1,5 +1,6 @@
 export function uploadFile(fileName: string, mimeType = "application/x-yaml") {
   cy.fixture(fileName).then(fileContent => {
+    cy.contains(`[role="button"]`, /load openapi/i).should("be.visible");
     cy.contains(`[role="button"]`, /load openapi/i).within(() => {
       cy.get("input").upload({
         fileContent,
