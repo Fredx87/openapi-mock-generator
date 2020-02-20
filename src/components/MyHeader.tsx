1091eb20d950ffe5933534455f54a341c0002917
Fredx <freddy87@gmail.com>
2020-02-21 00:01:21 +0100

    Added documentation link

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -1,3 +1,4 @@
+import Menu from "antd/es/menu";
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -17,10 +18,32 @@ const StyledLink = styled(Link)`
   }
 `;
 
+const StyledA = styled.a`
+  &&& {
+    color: #ddd;
+    line-height: 64px;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+`;
+
 export const MyHeader: React.FC = () => (
   <Container>
     <h1>
       <StyledLink to="/">{PAGE_TITLE}</StyledLink>
     </h1>
+    <Menu theme="dark" mode="horizontal">
+      <Menu.Item>
+        <StyledA
+          href="https://github.com/Fredx87/openapi-mock-generator/blob/master/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Documentation
+        </StyledA>
+      </Menu.Item>
+    </Menu>
   </Container>
 );
